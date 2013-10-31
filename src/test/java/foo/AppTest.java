6c528e039c984a7668076e50889183efeb26fa01
vad32m <vad32m@xaker.ru>
2013-10-31 16:31:27 +0200

    test fules updated

--- src/test/java/foo/AppTest.java
+++ src/test/java/foo/AppTest.java
@@ -9,13 +9,11 @@ public class AppTest
 	private BinaryTree tree;
     @Test
     public void testArea() 
-    
     {
          tree = new BinaryTree(15);
          assertTrue(tree.add(10));
-         assertFalse(tree.exists(3));
-         assertTrue(tree.remove(15));
-         assertFalse(tree.remove(10));
-         assertTrue(tree.exists(10));
+         assertTrue(tree.add(2));
+         assertFalse(tree.add(15));
+         assertTrue(tree.exists(15));
     }
 }
